c1280c11d0763bc813f715c597911d13bb823e26
Humberto Linero <17732781+hlinero@users.noreply.github.com>
2020-10-16 16:29:00 +0200

    Refactor the algorithm for generating the data of interest

--- app/main.py
+++ app/main.py
@@ -20,48 +20,46 @@ if not file_manager.is_data_file_available():
 
 # Get the dictionary with all in data to be analyzed
 all_variable_data = file_manager.get_variable_data()
-single_variable_data = all_variable_data["Single Variable"]
-multi_variable_data = all_variable_data["Multivariable"]
-cummulative_single_variable_data = all_variable_data["Cumulative Single Variable"]
 
-# Single variable Analysis
-for key in single_variable_data.keys():
-    variable = single_variable_data[key]
+# Create a dictionary were all dataframes will be stored
+dataframe_result_dict = {}
 
-    # Generate and save the input file to then be used for generating the graph
-    df = input_file_generator.get_dataframe_from_single_variable(variable)
-    file_manager.export_dataframe_for_variable(df, variable)
+# Generate a df for each item in the YAML file
+for analysis_type, value in all_variable_data.items():
+    for variable_key, variable_data in value.items():
+        # Printing progress
+        print(f"> {variable_key}")
+
+        # Set "False" as default
+        is_cumulative = False
+
+        # Generate a dataframe based on the analysis type
+        if analysis_type == "Single Variable":
+            df = input_file_generator.get_dataframe_from_single_variable(variable_data)
+        elif analysis_type == "Multivariable":
+            df = input_file_generator.get_dataframe_from_multi_variable(variable_data)
+        elif analysis_type == "Cumulative Single Variable":
+            is_cumulative = True
+            df = input_file_generator.get_dataframe_for_cummulative_single_variable(variable_data)
+            
+        # Save the dataframe into the dictionary
+        dataframe_result_dict[variable_key] = {"variable": variable_data, "dataframe": df, "is_cumulative": is_cumulative}
+
+# Export dataframe and generate graphs
+for data in dataframe_result_dict.values():
+    
+    # Get data of importance
+    df = data["dataframe"]
+    is_cumulative = data["is_cumulative"]
+    variable = data["variable"]
+
+    # Export the dataframe to a file
+    file_manager.export_dataframe_for_variable(df, variable, is_cumulative)
 
     # Generate the graph
-    path_to_input_file = file_manager.get_input_file_path_for_variable_name(variable)
+    path_to_input_file = file_manager.get_input_file_path_for_variable_name(variable, is_cumulative)
     figure = graphGenerator.get_figure_for_variable(variable, path_to_input_file)
-    file_manager.export_figure_for_variable(figure, variable)
-
-# Multivariable Analysis
-for key in multi_variable_data.keys():
-    variable = multi_variable_data[key]
-
-    # Generate and save the input file to then be used for generating the graph
-    df = input_file_generator.get_dataframe_from_multi_variable(variable)
-    file_manager.export_dataframe_for_variable(df, variable)
-
-    # Generate the graph
-    path_to_input_file = file_manager.get_input_file_path_for_variable_name(variable)
-    figure = graphGenerator.get_figure_for_variable(variable, path_to_input_file)
-    file_manager.export_figure_for_variable(figure, variable)
-
-# Cumulative - Single Variable Analysis
-for key in cummulative_single_variable_data.keys():
-    variable = cummulative_single_variable_data[key]
-
-    # Generate and save the input file to then be used for generating the graph
-    df = input_file_generator.get_dataframe_for_cummulative_single_variable(variable)
-    file_manager.export_dataframe_for_variable(df, variable, True)
-
-     # Generate the graph
-    path_to_input_file = file_manager.get_input_file_path_for_variable_name(variable, True)
-    figure = graphGenerator.get_figure_for_variable(variable, path_to_input_file)
-    file_manager.export_figure_for_variable(figure, variable, True)
+    file_manager.export_figure_for_variable(figure, variable, is_cumulative)
 
 # Print where to find the results
 print("All results saved at: {}".format(file_manager.path_to_save_results))
